e8c7912cb8421b75212f977bf2152df1cac421b5
Brian Norman <briankn8@gmail.com>
2019-12-17 17:28:37 -0800

    App Center setup

--- app/src/main/java/com/example/fleet/MainActivity.kt
+++ app/src/main/java/com/example/fleet/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.fleet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fleet.ui.main.MainFragment
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCenter.start(
+            application, BuildConfig.APPCENTER_API_KEY,
+            Analytics::class.java, Crashes::class.java
+        )
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
